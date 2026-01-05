ab941d923ba346389ba44f92db93131b2a079bf2
Rushit PC <rushitchandani@gmail.com>
2026-01-05 22:28:36 +0530

    update url

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 SD_API_URL = "https://53fa41f17ec0.ngrok-free.app/sdapi/v1/txt2img"
-SD_API_URL = "http://mink-noble-severely.ngrok-free.app:7860"
+SD_API_URL = "https://mink-noble-severely.ngrok-free.app"
 API_KEY = "sd-portfolio-key"
 
 DEFAULT_STEPS = 20
